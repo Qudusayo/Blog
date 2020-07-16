8bf5c88ab1d295afb7d61a59229e64766b053a74
Qudusayo <zeekimono@gmail.com>
2020-07-16 17:45:45 +0100

    Added   google tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-162659499-4`,
       },
     },
     `gatsby-plugin-feed`,
